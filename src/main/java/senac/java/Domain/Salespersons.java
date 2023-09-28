d539ead0d7d8e39bf384476d72726b37ef4a3686
Erisvan Barboza <erisvan.hybrid@gmail.com>
2023-09-27 21:55:09 -0300

    first commit

---
+++ src/main/java/senac/java/Domain/Salespersons.java
@@ -0,0 +1,13 @@
+package senac.java.Domain;
+
+public class Salesperson {
+
+    int id = 0;
+    String name = "";
+    String lastName = "";
+    String phoneNumber = "";
+    String cpf = "";
+    String email= "";
+    String address= "";
+
+}
---
+++ src/main/java/senac/java/Views/Telas.java
@@ -0,0 +1,16 @@
+package senac.java.Views;
+
+public class Opcoes {
+
+    public static void mostrarOpcoes(){
+
+        System.out.println("========== SISTEMA DE CADASTRO ==========");
+        System.out.println("OPÇÕES:");
+        System.out.println("1 - Cadastro de Vendedor");
+        System.out.println("2 - Cadastro de Cliente");
+        System.out.println("3 - Cadastro de Produto");
+        System.out.println("4 - Sair");
+    }
+
+
+}
