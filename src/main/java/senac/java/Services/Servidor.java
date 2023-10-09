2705e167bedcb7069e9f2bd973552649b826df37
Erisvan Barboza <erisvan.hybrid@gmail.com>
2023-10-09 19:57:57 -0300

    Métodos GET, PUT, POST, DELETE e NÃo DISPONÍVEL

--- src/main/java/senac/java/Services/Servidor.java
+++ src/main/java/senac/java/Services/Servidor.java
@@ -16,31 +16,35 @@ public class Servidor {
         Users user = new Users();
 
         HttpServer server = HttpServer.create(new InetSocketAddress(3000), 0);
-        
-        server.createContext("/teste", (HttpExchange exchange) -> {
-            if ("GET".equals(exchange.getRequestMethod())){
 
-                String response = user.name + " " + user.lastName;
-                exchange.sendResponseHeaders(200, response.getBytes().length);
-
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-
-            } else {
-
-                String response = "Método não implementado";
-                exchange.sendResponseHeaders(405, response.length());
-
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-            }
-        });
+        server.createContext("/api/usuario", Servidor::chamar);
 
         server.setExecutor(null);
         server.start();
         System.out.println("Servidor iniciado");
+
+    }
+
+    public static void chamar(HttpExchange exchange) throws IOException{
+
+        if ("GET".equals(exchange.getRequestMethod())){
+
+            String response = "Cheguei no método chamar";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+
+        } else {
+
+            String response = "Método não implementado";
+            exchange.sendResponseHeaders(405, response.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
 
     }
 }
